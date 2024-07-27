a92ca897e407f0c6730dea0233c4a9ffd2a5e3a5
DenVik <denyagarkusha2@gmail.com>
2024-07-27 14:21:08 +0200

    theme

--- main.js
+++ main.js
@@ -13,4 +13,25 @@ menuList.classList.remove('menu-list-open');
 mobileBg.classList.remove('.mobile-menu-bg-open');
 });
 
+const darkBtn = document.querySelector('.dark');
+const whiteBtn = document.querySelector('.white');
+const body = document.querySelector('.body');
+const footer = document.querySelector('.footer');
+const coment = document.querySelector('.section-coment');
+darkBtn.addEventListener('click', () => {
+	body.classList.add('dark-theme');
+	darkBtn.classList.add('is-close');
+	whiteBtn.classList.add('is-open');
+	coment.classList.add('dark-theme')
+	footer.classList.add('dark-theme')
+});
+
+
+whiteBtn.addEventListener('click', () => {
+	body.classList.remove('dark-theme');
+	darkBtn.classList.remove('is-close');
+	whiteBtn.classList.remove('is-open');
+	coment.classList.remove('dark-theme')
+	footer.classList.remove('dark-theme')
+});
 
